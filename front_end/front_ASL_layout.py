99c02460b55c0d902dc6300edc74681bed55f780
Bo Niehaus <109623921+just1984@users.noreply.github.com>
2024-11-22 14:15:17 +0100

    Update front_ASL_layout.py

--- front_end/front_ASL_layout.py
+++ front_end/front_ASL_layout.py
@@ -7,7 +7,7 @@ import base64
 def display_image_columns(processed_image, hand_region, prediction_and_confidence):
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.image(processed_image, caption="Original", use_column_width=True)
+        st.image(processed_image, caption="Original", use_container_width=True)
     with col2:
         st.image(hand_region, caption="Hand region")
     with col3:
@@ -23,13 +23,14 @@ def adjust_brightness_contrast(image, brightness=40, contrast=1.0):
     """
     Adjust the brightness and contrast of an image.
     """
-    image = image.getvalue()
-    image = Image.open(BytesIO(image))
-    image = np.array(image)
+    if not isinstance(image, Image.Image):
+        image = Image.open(BytesIO(image.getvalue()))
 
-    img = image.astype(np.float32)
+    img = np.array(image).astype(np.float32)
+
     img = img * contrast + brightness
     img = np.clip(img, 0, 255).astype(np.uint8)
 
+    adjusted_image = Image.fromarray(img)
 
-    return img
+    return adjusted_image
