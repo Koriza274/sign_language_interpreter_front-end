bd2a95685e612c1f21fe782689c279bff61cb6c2
Bo Niehaus <109623921+just1984@users.noreply.github.com>
2024-11-22 15:59:57 +0100

    Update video_section.py

--- front_end/video_section.py
+++ front_end/video_section.py
@@ -2,20 +2,20 @@ import os
 import streamlit as st
 
 def display_video_section(video_path, word):
-    """
-    Displays the video section for the given word, allowing the user to learn how to sign it.
-    """
-    st.title(f"Learn to sign the word: {word}")
+    ##placeholder to display the HIPPO video
+#    word = "HIPPO"
+    st.markdown(f'Do you want to learn how to sign the word {word}?')
+
     st.video(os.path.join(video_path, f'{word}.mp4'), start_time=0.10)
 
     lbl = f'Record your {word} sign video, or upload a video'
     camera_input = st.camera_input(label=lbl, key=f"camera_3_{st.session_state.page_key['Game On!']}")
 
-    uploaded_file = st.file_uploader("Upload your sign video", type=["mp4"])
+#    uploaded_file = st.file_uploader("Upload video", type=["jpg", "jpeg", "png"])
+    uploaded_file = st.file_uploader("", type=["mp4"])
 
     if camera_input:
-        st.markdown("Sending video to API for prediction...")
+        st.markdown("Sending video to API for prediction")
 
     if uploaded_file:
-        st.markdown("Sending uploaded video to API for prediction...")
-
+        st.markdown("Sending video to API for prediction")
