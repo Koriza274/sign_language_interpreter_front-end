5b6e82dc04ace325ef1ab9133f6f8e3666d76142
Diana <168603138+Koriza274@users.noreply.github.com>
2024-11-22 18:09:12 +0300

    Update video_section.py

--- front_end/video_section.py
+++ front_end/video_section.py
@@ -9,13 +9,13 @@ def display_video_section(video_path, word):
     st.video(os.path.join(video_path, f'{word}.mp4'), start_time=0.10)
 
     lbl = f'Record your {word} sign video, or upload a video'
-    camera_input = st.camera_input(label=lbl, key=f"camera_3_{st.session_state.page_key['Game On!']}")
+    #camera_input = st.camera_input(label=lbl, key=f"camera_3_{st.session_state.page_key['Game On!']}")
 
 #    uploaded_file = st.file_uploader("Upload video", type=["jpg", "jpeg", "png"])
     uploaded_file = st.file_uploader("", type=["mp4"])
 
-    if camera_input:
-        st.markdown("Sending video to API for prediction")
+    #if camera_input:
+        #st.markdown("Sending video to API for prediction")
 
     if uploaded_file:
         st.markdown("Sending video to API for prediction")
